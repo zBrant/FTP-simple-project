9ccc8e69fe0b39b68b77251ea77395ac969a09ab
EduardoBrant <carlos23brant@hotmail.com>
2024-06-18 00:42:09 -0400

    Correct name now

--- ftp-app/src/main/java/io/github/zbrant/Main.java
+++ ftp-app/src/main/java/io/github/zbrant/Main.java
@@ -6,13 +6,13 @@ import io.github.zbrant.core.utils.FormatterUtils;
 public class Main {
 
   public static void main(String[] args) throws Exception {
-    if(!valideInput(args)) return;
+    if(!validInput(args)) return;
     String serverIp = args[0];
     Integer serverPort = 21;
     FTPClientApp.start(serverIp, serverPort);
   }
 
-  public static boolean valideInput(String[] args){
+  public static boolean validInput(String[] args){
     if(args.length != 1){
       FormatterUtils.printHelper();
       return false;
