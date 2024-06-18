8b54ea10a60268a4ea04f1190b15f989148f8e37
Raul <raul.cfr112@gmail.com>
2024-06-17 20:13:26 -0400

    Email implementation.

--- ftp-app/src/main/java/io/github/zbrant/Main.java
+++ ftp-app/src/main/java/io/github/zbrant/Main.java
@@ -1,13 +1,19 @@
 package io.github.zbrant;
 
 import io.github.zbrant.core.FtpClient;
+import io.github.zbrant.core.integration.email.EmailSenderImpl;
 import org.apache.commons.net.ftp.FTPClient;
 
 public class Main {
   public static void main(String[] args) throws Exception {
     System.out.println("Hello from client!");
 
-    FtpClient service = new FtpClient();
+    EmailSenderImpl emailService = new EmailSenderImpl("raul.cfr112@gmail.com",
+            "trabalhoredeswork@gmail.com",
+            "smtp.gmail.com",
+            "C:\\Users\\Raul\\Documents\\FTP-simple-project\\textTest.txt"
+    );
+//    FtpClient service = new FtpClient();
     //FTPClient client = service.loginFtp("172.17.0.2", 21, "ftpuser", "passwd");
     //service.printTree("./", client);
     //client.logout();
--- ftp-app/src/main/java/io/github/zbrant/core/integration/email/EmailSenderImpl.java
+++ ftp-app/src/main/java/io/github/zbrant/core/integration/email/EmailSenderImpl.java
@@ -1,37 +1,51 @@
 package io.github.zbrant.core.integration.email;
 
-import javax.mail.Message;
-import javax.mail.Session;
-import javax.mail.Transport;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.*;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
-import java.io.OutputStream;
+import javax.mail.internet.MimeMultipart;
 import java.util.Properties;
 
 public class EmailSenderImpl {
 
-  /*
-    TODO:
-     - enviar anexo junto do email
-   */
+    public EmailSenderImpl(String to, String from, String host, String fileName) {
+        Properties properties = System.getProperties();
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtps.auth", "true");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.smtp.host", host);
+        Session session = Session.getDefaultInstance(properties, null);
 
-  public EmailSenderImpl(String to, String from, String host, OutputStream file) {
-    Properties properties = System.getProperties();
-    properties.setProperty("mail.smtp.host", host);
-    Session session = Session.getDefaultInstance(properties);
 
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject("Ping");
+            message.setText("Hello, this is example of sending email  ");
+            BodyPart messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setText("Here's the file");
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(messageBodyPart);
+            messageBodyPart = new MimeBodyPart();
+            DataSource source = new FileDataSource(fileName);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(fileName);
+            multipart.addBodyPart(messageBodyPart);
+            message.setContent(multipart);
 
-    try {
-      MimeMessage message = new MimeMessage(session);
-      message.setFrom(new InternetAddress(from));
-      message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-      message.setSubject("Ping");
-      message.setText("Hello, this is example of sending email  ");
-
-      Transport.send(message);
-      System.out.println("message sent successfully....");
-    } catch (Exception e) {
-      System.out.println("Error: " + e.getMessage());
+            Transport transport = session.getTransport();
+            transport.connect(host, 587, from, "telobnvdwczjyrfb ");
+            transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO));
+            transport.close();
+            System.out.println("message sent successfully....");
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
-  }
 }
